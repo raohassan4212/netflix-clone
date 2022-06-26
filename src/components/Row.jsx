cde6754f8be9e211ebef1ab17065c30e527cd62a
raohassan4212 <52700173+raohassan4212@users.noreply.github.com>
2022-06-26 11:47:26 -0700

    Initial commit

---
+++ src/components/Row.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import axios from "../axios";
+import "./Row.css"
+
+const baseUrl = "https://image.tmdb.org/t/p/original/";
+
+const Row = ({ title, fetchUrl }) => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  console.table(movies);
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row-posters">
+        {movies.map((movie) => {
+          return (
+            <img
+              className="row-poster"
+              src={`${baseUrl}${movie.poster_path}`}
+              alt={movie.title}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
