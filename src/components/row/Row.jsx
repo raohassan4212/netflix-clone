9a2c511fdd7ce8c42fa76ff277fec3fc3e6becbe
raohassan4212 <52700173+raohassan4212@users.noreply.github.com>
2022-06-28 00:54:02 -0700

    add two packages react-youtube and movie-trailer to add functionality when we click on thumbnail it show the movie trailer

--- src/components/row/Row.jsx
+++ src/components/row/Row.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import axios from "../../axios";
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const baseUrl = "https://image.tmdb.org/t/p/original/";
 
 const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
@@ -14,6 +17,32 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
     }
     fetchData();
   }, [fetchUrl]);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  function handleClick(movie) {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    }
+    else {
+      movieTrailer(movie?.name || "")
+      .then((url) => {
+        const urlParams = new URLSearchParams(new URL(url).search);
+        setTrailerUrl(urlParams.get("v"));
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    }
+  }
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -27,10 +56,12 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
                 isLargeRow ? movie.poster_path : movie.backdrop_path
               }`}
               alt={movie.title}
+              onClick={() => handleClick(movie)}
             />
           );
         })}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
